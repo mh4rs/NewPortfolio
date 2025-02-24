6806a03bdee906a8c8592f765ada8341f19ad126
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2025-02-24 20:10:05 +0800

    refactor: adjust components structure (#53)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [ ] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [ ] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Release (new release)
    - [x] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Update the current version of ui components 
    
    ###
    
    # Related Tickets
    
    <!-- _If this pull request contains related or close an issue, please
    add them here. -->
    
    - Related Issue(s) #
    - Closes #
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- components/common/Cldpicture.tsx
+++ components/common/Cldpicture.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Image from "@node_modules/next/image";
+import { CldImage } from "next-cloudinary";
+import { Url } from "next/dist/shared/lib/router/router";
 
 type Props = {
-  src: string;
+  src: string | Url;
   alt: string;
   isPriority?: boolean;
   className?: string;
@@ -11,9 +12,9 @@ type Props = {
 
 export default function Picture({ src, alt, isPriority, className }: Props) {
   return (
-    <Image
+    <CldImage
       priority={isPriority}
-      src={src}
+      src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1710343815/portfolio/${src}`}
       fill
       sizes="(min-width: 475px) 100vw, (min-width: 768px) 1080px, (min-width: 1024px) 1920px"
       alt={`${alt}`}
